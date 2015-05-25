c8a2b70c8939951264749c54127ecd5139846bf2
mwhouser <matt@eleven41.com>
2015-05-25 19:14:44 -0400

    Added Lambda function event handler

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # aws-lambda-encrypt-s3-objects
-An AWS Lambda function to encrypt S3 objects using server-side AES256 encryption as they are added to the bucket.
+An AWS Lambda function to encrypt S3 objects using server-side AES256 encryption 
+as they are added to the bucket.
+
+## Configuration
+
+### IAM Role
+
+Create an IAM role with the following policy:
+
+```json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1430872797000",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Sid": "Stmt1430872844000",
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:*"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Sid": "Stmt1430872852000",
+            "Effect": "Allow",
+            "Action": [
+                "logs:*"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+```
+
+### Lambda Function
+
+1. Create a new Lambda function.
+2. Upload the file index.js as the code for your Lambda function.
+3. Add an event source to your Lambda function:
+ * Event Source Type: S3
+ * Bucket: your source bucket
+ * Event Type: Object Created
+
+At this point, if you upload a file to your source bucket, the file 
+should be converted to AES256 encryption if it isn't already encrypted.
---
+++ index.js
@@ -0,0 +1,57 @@
+﻿// Load up all dependencies
+var AWS = require('aws-sdk');
+
+console.log("Version 0.1.0");
+
+// get reference to S3 client 
+var s3 = new AWS.S3();
+
+// This is the entry-point to the Lambda function.
+exports.handler = function (event, context) {
+    
+    // Process all records in the event.
+    for (var i = 0; i < event.Records.length; ++i) {
+        
+        // The bucket and key are part of the event data
+        var bucket = event.Records[i].s3.bucket.name;
+        var key = unescape(event.Records[i].s3.object.key);
+        
+        console.log('Processing ' + bucket + '/' + key);
+
+        // Get the head data to determine if the object is already encrypted.
+        console.log('Getting object head');
+        s3.headObject({
+            Bucket: bucket,
+            Key: key
+        }, function (err, data) {
+            if (err) {
+                console.log('Error getting object head:');
+                console.log(err, err.stack); // an error occurred
+                context.fail('Error', "Error getting object head: " + err);
+            } else if (data.ServerSideEncryption != 'AES256') {
+                // Copy the object adding the encryption
+                console.log('Updating object');
+                s3.copyObject({
+                    Bucket: bucket,
+                    Key: key,
+                    
+                    CopySource: escape(bucket + '/' + key),
+                    MetadataDirective: 'COPY',
+                    ServerSideEncryption: 'AES256'
+                }, function (err, data) {
+                    if (err) {
+                        console.log('Error updating object:');
+                        console.log(err, err.stack); // an error occurred
+                        context.fail('Error', "Error updating object: " + err);
+                    } else {
+                        console.log('Object updated.');
+                        context.succeed();
+                    }
+                });
+            } else {
+                console.log("Object is already encrypted using 'AES256'.");
+                context.succeed();
+            }
+        });
+    }
+};
